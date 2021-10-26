3ffffab3b95bce5be54001b631adeb2dbfbae5fe
Bruna Silva <brunasgf@gmail.com>
2021-10-26 14:57:56 -0300

    Update quique.js

--- quique.js
+++ quique.js
@@ -1,7 +1,7 @@
 let widget;
 const startBot = () => {
     widget = new BlipChat()
-        .withAppKey('cm91dGVyY2VsdWxhb3dlbnM6ZjVhYmU0MWYtMzc2MS00MDRjLWFhYzQtMjQ1YTQ1Njg3MzIy')
+        .withAppKey('{YOUR_TOKEN_HERE}')
         .withButton({ "color": "#2CC3D5", "icon": "" })
         .withCustomCommonUrl('https://take.chat.blip.ai/')
         .build();
